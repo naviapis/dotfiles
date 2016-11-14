2da3e465d72facb3182550a23a1eecb0641777a5
Haruki Fukui <haruki.fukui@naviapis.com>
2016-11-15 01:20:29 +0900

    update commit

--- .oh-my-zsh-custom/aliases.zsh
+++ .oh-my-zsh-custom/aliases.zsh
@@ -1,5 +1,4 @@
 alias vi='vim'
-alias sed='gsed'
 # List direcory contents
 alias ls='gls --color=auto'
 alias l.='gls --color=auto -d .*'
--- .oh-my-zsh-custom/main.zsh
+++ .oh-my-zsh-custom/main.zsh
@@ -3,23 +3,18 @@ if [ -e $(brew --prefix)/share/zsh-syntax-highlighting/zsh-syntax-highlighting.z
     source $(brew --prefix)/share/zsh-syntax-highlighting/zsh-syntax-highlighting.zsh
 fi
 
-# Ruby
-if $(brew ls --versions ruby &> /dev/null); then
-    export PATH="$(brew --prefix ruby)/bin:$PATH"
-fi
-
 # PHP
-if $(brew ls --versions php56 &> /dev/null); then
-    export PATH="$(brew --prefix homebrew/php/php56)/bin:$PATH"
-fi
+#if $(brew ls --versions php56 &> /dev/null); then
+#    export PATH="$(brew --prefix homebrew/php/php56)/bin:$PATH"
+#fi
 
 # Java Development Kit
 export JAVA_HOME=$(/usr/libexec/java_home)
 
 # Android SDK
-if $(brew ls --versions android-sdk &> /dev/null); then
-    export ANDROID_HOME=$(brew --prefix android-sdk)
-fi
+#if $(brew ls --versions android-sdk &> /dev/null); then
+#    export ANDROID_HOME=$(brew --prefix android-sdk)
+#fi
 
-eval `gdircolors $HOME/.dircolors`
+#eval `gdircolors $HOME/.dircolors`
 zstyle ':completion:*' list-colors ${(s.:.)LS_COLORS}
