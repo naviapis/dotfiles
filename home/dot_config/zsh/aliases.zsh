e756ace6cf77c438fb402ff8f260484ef6d21c72
Haruki Fukui <haruki.fukui@naviapps.com>
2025-09-15 08:43:53 +0900

    update commit

--- home/dot_config/zsh/aliases.zsh
+++ home/dot_config/zsh/aliases.zsh
@@ -1,7 +1,4 @@
-alias rm='trash'
-alias xdg-ninja='xdg-ninja --skip-unsupported'
-
-# Rust
 alias cat='bat'
 alias ls='eza'
-alias http='xh'
+alias vim='nvim'
+alias xdg-ninja='xdg-ninja --skip-unsupported'
--- home/dot_config/zsh/xdg-base-directory.zsh
+++ home/dot_config/zsh/xdg-base-directory.zsh
@@ -1,14 +1,11 @@
 # XDG Base Directory
 # https://wiki.archlinux.jp/index.php/XDG_Base_Directory
 
+# Claude Code
+export CLAUDE_CONFIG_DIR="$XDG_CONFIG_HOME"/claude
+
 # Docker
 export DOCKER_CONFIG="$XDG_CONFIG_HOME"/docker
 
 # npm
-export NPM_CONFIG_USERCONFIG=$XDG_CONFIG_HOME/npm/npmrc
-
-# xdg-ninja
-# https://github.com/b3nj5m1n/xdg-ninja
-
-# .lesshst
-export LESSHISTFILE="$XDG_STATE_HOME"/lesshst
+export NPM_CONFIG_USERCONFIG="$XDG_CONFIG_HOME"/npm/npmrc
